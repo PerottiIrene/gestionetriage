5a1ff925c2e5b341c8f9ea83b89cd52f234cd6eb
PerottiIrene <ireneperotti29@gmail.com>
2022-12-01 22:11:59 +0100

    implementate classi di model

---
+++ src/main/java/it/prova/gestionetriage/model/Paziente.java
@@ -0,0 +1,107 @@
+package it.prova.gestionetriage.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "paziente")
+public class Paziente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "nome")
+	private String nome;
+	@Column(name = "cognome")
+	private String cognome;
+	@Column(name = "codiceFiscale")
+	private String codiceFiscale;
+	@Column(name = "dataRegistrazione")
+	private Date dataRegistrazione;
+	@Column(name = "codiceDottore")
+	private String codiceDottore;
+	
+	@Enumerated(EnumType.STRING)
+	private StatoPaziente stato;
+	
+	public Paziente() {}
+
+	public Paziente(Long id, String nome, String cognome, String codiceFiscale, Date dataRegistrazione,
+			String codiceDottore, StatoPaziente stato) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.codiceFiscale = codiceFiscale;
+		this.dataRegistrazione = dataRegistrazione;
+		this.codiceDottore = codiceDottore;
+		this.stato = stato;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getCodiceFiscale() {
+		return codiceFiscale;
+	}
+
+	public void setCodiceFiscale(String codiceFiscale) {
+		this.codiceFiscale = codiceFiscale;
+	}
+
+	public Date getDataRegistrazione() {
+		return dataRegistrazione;
+	}
+
+	public void setDataRegistrazione(Date dataRegistrazione) {
+		this.dataRegistrazione = dataRegistrazione;
+	}
+
+	public String getCodiceDottore() {
+		return codiceDottore;
+	}
+
+	public void setCodiceDottore(String codiceDottore) {
+		this.codiceDottore = codiceDottore;
+	}
+
+	public StatoPaziente getStato() {
+		return stato;
+	}
+
+	public void setStato(StatoPaziente stato) {
+		this.stato = stato;
+	}
+
+	
+}
---
+++ src/main/java/it/prova/gestionetriage/model/StatoPaziente.java
@@ -0,0 +1,6 @@
+package it.prova.gestionetriage.model;
+
+public enum StatoPaziente {
+	IN_ATTESA_VISITA, IN_VISITA, RICOVERATO, DIMESSO
+
+}
